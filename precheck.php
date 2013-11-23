610913b2f1edb86522985c25c16794a701475fb4
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-11-23 17:34:29 +0100

    don't check for Dwoo at BlackCat CMS

--- precheck.php
+++ precheck.php
@@ -4,8 +4,8 @@
  * dbMultipleChoice
  *
  * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
- * @link https://addons.phpmanufaktur.de/de/name/dbmultiplechoice.php
- * @copyright 2010 - 2012 phpManufaktur by Ralf Hertsch
+ * @link https://phpmanufaktur.de
+ * @copyright 2010 - 2013 phpManufaktur by Ralf Hertsch
  * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
 
@@ -34,19 +34,18 @@ else {
 // Checking Requirements
 
 $PRECHECK['PHP_VERSION'] = array('VERSION' => '5.2.0', 'OPERATOR' => '>=');
-$PRECHECK['WB_ADDONS'] = array(
-	'dbconnect_le'	=> array('VERSION' => '0.70', 'OPERATOR' => '>='),
-	'dwoo' => array('VERSION' => '0.11', 'OPERATOR' => '>=')
-);
+$PRECHECK['WB_ADDONS']['dbconnect_le'] = array('VERSION' => '0.71', 'OPERATOR' => '>=');
+if (!defined('CAT_VERSION')) {
+    $PRECHECK['WB_ADDONS']['dwoo'] = array('VERSION' => '0.17', 'OPERATOR' => '>=');
+}
 
 global $database;
+
 $sql = "SELECT * FROM ".TABLE_PREFIX."settings WHERE name='default_charset'";
 $result = $database->query($sql);
 if ($result) {
-	$data = $result->fetchRow();
-	($data['value'] == 'utf-8') ? $status = true : $status = false;
-	$PRECHECK['CUSTOM_CHECKS'] = array(
+    $data = $result->fetchRow();
+    ($data['value'] == 'utf-8') ? $status = true : $status = false;
+    $PRECHECK['CUSTOM_CHECKS'] = array(
     'Default Charset' => array('REQUIRED' => 'utf-8', 'ACTUAL' => $data['value'], 'STATUS' => $status));
 }
-
-?>
