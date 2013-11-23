93e9eabc8d99c5d1e012dbffea4e0a7f8a65cddf
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-11-23 17:33:54 +0100

    changed check for Dwoo

--- initialize.php
+++ initialize.php
@@ -31,32 +31,33 @@ else {
 }
 // end include class.secure.php
 
-if (!defined('DEBUG_MODE')) define('DEBUG_MODE', true);
+if (!defined('DEBUG_MODE')) define('DEBUG_MODE', false);
 
 if (DEBUG_MODE) {
-	ini_set('display_errors', 1);
-	error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
 }
 else {
-	ini_set('display_errors', 0);
-	error_reporting(E_ERROR);
+    ini_set('display_errors', 0);
+    error_reporting(E_ERROR);
 }
 
 // include language file
 if(!file_exists(WB_PATH .'/modules/'.basename(dirname(__FILE__)).'/languages/' .LANGUAGE .'.php')) {
-	require_once(WB_PATH .'/modules/'.basename(dirname(__FILE__)).'/languages/EN.php');
+    require_once(WB_PATH .'/modules/'.basename(dirname(__FILE__)).'/languages/EN.php');
 }
 else {
-	require_once(WB_PATH .'/modules/'.basename(dirname(__FILE__)).'/languages/' .LANGUAGE .'.php');
+    require_once(WB_PATH .'/modules/'.basename(dirname(__FILE__)).'/languages/' .LANGUAGE .'.php');
 }
 
 // include dbConnect
 require_once(WB_PATH.'/modules/dbconnect_le/include.php');
-require_once(WB_PATH.'/modules/dwoo/include.php');
+
+if (!class_exists('Dwoo')) {
+    require_once(WB_PATH.'/modules/dwoo/include.php');
+}
 require_once(WB_PATH.'/modules/'.basename(dirname(__FILE__)).'/class.config.php');
 require_once(WB_PATH.'/modules/'.basename(dirname(__FILE__)).'/class.multiplechoice.php');
 
 global $parser;
 if (!is_object($parser)) $parser = new Dwoo();
-
-?>
